0d24554d9208139105acbae3f9b80aa7ee50120d
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-02-21 23:50:01 +0530

    i have made another changes #1823

--- Ex4.java
+++ Ex4.java
@@ -1,10 +1,11 @@
 public class Ex4 {
     public static boolean isPalindrome(String str) {
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int left = 0;
-        int right = str.length() - 1;
+        int right = cleanedStr.length() - 1;
         
         while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
+            if (cleanedStr.charAt(left) != cleanedStr.charAt(right)) {
                 return false;
             }
             left++;
@@ -14,7 +15,7 @@ public class Ex4 {
     }
 
     public static void main(String[] args) {
-        String testStr = "racecar";
+        String testStr = "A man, a plan, a canal, Panama";
         System.out.println("Is '" + testStr + "' a palindrome? " + isPalindrome(testStr));
     }
 }
