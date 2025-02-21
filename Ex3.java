afbeb1ce5b5823a71ea428fcb4ddec32e6c3a865
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-02-21 22:04:51 +0530

    second commit message from vs code UI

---
+++ Ex3.java
@@ -0,0 +1,23 @@
+public class Ex3 {
+    public static void main(String[] args) {
+        int num = 20;
+        System.out.println("Prime numbers up to " + num + " are: ");
+        for (int i = 2; i <= num; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
